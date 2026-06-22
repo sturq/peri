91d2c8e171e874029e307c841e3cddedec7b45f6
sturq <sturq@users.noreply.github.com>
2026-06-22 18:26:49 +0200

    Remove unused Canvas import

--- app/src/main/java/app/grapheneos/camera/AppleLook.kt
+++ app/src/main/java/app/grapheneos/camera/AppleLook.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.BitmapShader
-import android.graphics.Canvas
 import android.graphics.ColorSpace
 import android.graphics.HardwareRenderer
 import android.graphics.Paint
